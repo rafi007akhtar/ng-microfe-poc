1543831a3e83991d3fce68ad23014205e7f0dadb
Md Rafi Akhtar <alimdrafi@gmail.com>
2025-01-08 00:17:20 +0530

    correction and update corrections from previous commit: - changes were only mostly done to the first remote app - vite was set up in the app, and a component was defined and exposed in   the vite config
    update:
    - the same was done for remote app 2 in this commit, along with the
      angular plugin from analogjs

---
+++ remote-app-2/vite.config.ts
@@ -0,0 +1,22 @@
+import angular from '@analogjs/vite-plugin-angular';
+import { defineConfig } from 'vite';
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineConfig({
+  plugins: [
+    angular(),
+    federation({
+      name: 'remoteApp2',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './RemoteComponent2': './src/app/cmp2/cmp2.component.ts',
+      },
+      shared: ['@angular/core', '@angular/common', '@angular/router'],
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
