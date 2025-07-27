3bd902867b4a395a190717b152659e7c1a07cafd
Md Rafi Akhtar <alimdrafi@gmail.com>
2025-07-27 20:31:15 +0530

    clean slate with the new branch

--- host-app/src/module-federation.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'remoteApp1/RemoteModule' {
-  export * from 'src/app/remote/remote.module';
-}
-
-declare module 'remoteApp2/RemoteModule' {
-  export * from 'src/app/remote/remote.module';
-}
