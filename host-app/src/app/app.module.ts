152b4cec008b42a772aa4d456c3b92c5c331287c
Md Rafi Akhtar <alimdrafi@gmail.com>
2025-08-15 12:11:15 +0530

    configured host app routing and component

--- host-app/src/app/app.module.ts
+++ host-app/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'remote1',
+    loadChildren: () =>
+      import('remote1/Remote1Module' as any).then((m) => m.Remote1Module),
+  },
+  {
+    path: 'remote2',
+    loadChildren: () =>
+      import('remote2/Remote2Module' as any).then((m) => m.Remote2Module),
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    // AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
