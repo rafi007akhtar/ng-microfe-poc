200aff45fb48827029d79cfe47cfc05941701600
Md Rafi Akhtar <alimdrafi@gmail.com>
2025-08-16 17:40:37 +0530

    Injected service in app component of host to load AppComponent of remote. It worked! The remote is loading in the host

--- movie-ticket/src/app/app.component.ts
+++ movie-ticket/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { MfHelpersService } from './mf-helpers.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'movie-ticket';
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('movieList', {
+    read: ViewContainerRef,
+    static: true,
+  })
+  listContainer!: ViewContainerRef;
+
+  private listComponentRef: ComponentRef<any> | null = null;
+
+  constructor(private mfHelpers: MfHelpersService) {}
+
+  async ngOnInit(): Promise<void> {
+    const moudleName = 'movie-list';
+    const [listModule, error] = await this.mfHelpers.loadRemoteComponent({
+      name: moudleName,
+      port: 4201,
+    });
+    if (error) {
+      console.error(`Error loading moudle ${moudleName}: `, error);
+      return;
+    }
+
+    this.listContainer.clear();
+    this.listComponentRef = this.listContainer.createComponent(
+      listModule.AppComponent
+    );
+    this.listComponentRef.changeDetectorRef.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.listComponentRef?.destroy();
+  }
 }
